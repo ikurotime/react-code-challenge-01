ddae4fe0f8656b2f6ec0d390b6fd9b33adde8887
David Huertas <davidhuertasortiz@gmail.com>
2023-10-24 19:26:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/*
+  - TECHNICAL INTERVEW - REACT JUNIOR
+  Somos unos fanáticos de los gatos y queremos enseñarle al resto lo bonitos que son.
+  Vamos a crear una página donde podamos recopilar los datos de nuestros gatos favoritos.
+  En nuestra página podremos además consultar información de datos curiosos sobre gatos
+
+  Demuestra tus habilidades con React y haz una página con la información de estas APIs:
+  https://cataas.com/
+  https://catfact.ninja/fact
+
+  Puedes consultar información, asi como la documentación en cualquier momento
+  El resultado tiene que mostrar la imagen del gato a la izquierda y el texto a la derecha
+  centrado verticalmente
+
+  REQUISITOS
+  - Realizar llamada a la API y sacar la información en un componente (10 gatos)[]
+  - Darle nombre a los gatos y mostrarlo junto a su foto []
+  - Mostrar datos curiosos de los gatos []
+*/
+
+function App() {
+  return <>{/* YOUR CODE HERE */}</>
+}
+
+export default App
